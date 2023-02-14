186ade8fd8e2e34940c1888fa446170904db740c
Lucas Calabria <lucascalabria86@gmail.com>
2023-02-14 15:41:23 -0300

    Auth

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -34,7 +34,7 @@ export class UserController {
     public signup = async (req: Request, res: Response) => {
         try {
             const input: SignupInput = {
-                id: req.body.id,
+                // id: req.body.id,
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password
